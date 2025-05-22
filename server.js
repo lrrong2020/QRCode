58d20ec12c1ad1bc159369fd089e2fb3ed6d13c4
activikid <activikid.ai@gmail.com>
2025-05-22 15:34:49 +0800

    Add .gitignore for node_modules and comment out body logging in server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
   // console.log('Headers:', req.headers);
   // console.log('Query Parameters:', req.query);
   if (req.method !== 'GET') {
-    console.log('Body:', req.body);
+    // console.log('Body:', req.body);
   }
   next();
 });
